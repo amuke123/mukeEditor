da9d6f9b37e88692d7511e0f2f8017df2b069a63
Muke <amuke123@foxmail.com>
2020-12-02 17:06:22 +0800

    更新图标
    更新图标

--- lang/zh-CN.js
+++ lang/zh-CN.js
@@ -1,3 +1,6 @@
-var lang={
+MK.AddFunc({
 	name:"243243第三个",
-};
+	bold:"加粗",
+	italic:"斜体",
+	strike:"删除线",
+});
--- mukeEditor.js
+++ mukeEditor.js
@@ -1,37 +1,78 @@
 //console.log(document.currentScript.src);
 (function (window){
 	window.MK={
-		M:'',
-		langs:{},
+		//lang:{},
+		langstr:'',
 		language:'zh-CN',
 		style:'style',
 		Path:'',
+		navs:[
+			'html','|','bold','italic','delline','underline','fontSize','fontFamily','paragraph','color','backColor','|',
+			'left','center','right','full','indent','outdent','|','link','unlink','textBlock','code','selectAll','removeStyle','removeHtml','|',
+			'image','audio','video','subscript','superscript','hr','orderedList','unorderedList','|','undo','redo','|','about',
+		],
 		getEditor(box){
-			MK.M = document.getElementById(box);
-			MK.setDir();
-			MK.loadCss(MK.style);
-			MK.loadScript(MK.language);
-			//MK.langs=new lang();
-			console.log(lang.name);
-			return MK;
+			K=MK.Loading(box);
+			//console.table(K);
+			return K;
 		},
-		loadCss(el){
+		LoadCss(el){
 			var css=document.createElement('link');
 			css.type="text/css",
 			css.rel="stylesheet",
 			css.href=MK.Path+'themes/css/'+el+'.css';
 			var h=document.getElementsByTagName("head");
 			h.length ? h[0].appendChild(css):document.documentElement.appendChild(css);
+			
 		},
-		loadScript(el){
+		LoadScript(el){
 			var script=document.createElement('script');
 			script.type="text/javascript",
 			script.src=MK.Path+'lang/'+el+'.js';
 			var h=document.getElementsByTagName("head");
-			h.length ? h[0].appendChild(script):document.documentElement.appendChild(script);
+			h.length?h[0].appendChild(script):document.documentElement.appendChild(script);
 		},
-		setDir(){var mydir=document.currentScript.src;MK.Path=mydir.substring(8,mydir.lastIndexOf("\/")+1);},
+		SetDir(){
+			var els=document.getElementsByTagName('script'),src;
+			for(var i=0,len=els.length;i<len;i++){
+				src=els[i].src||'';
+				if(/mukeEditor[\w\-\.]*\.js/.test(src)){
+					MK.Path=src.substring(0,src.lastIndexOf('/')+1);
+				}
+			}
+		},
+		AddFunc(data){MK.lang=data;},
+		Loading(box){
+			MK.SetDir();
+			MK.LoadCss(MK.style);
+			MK.LoadCss('icono');
+			MK.LoadScript(MK.language);
+			MK.DisPlay(box);
+			return MK;
+		},
+		getNavList(){
+			var texts='';
+			for(i=0,len=MK.navs.length;i<len;i++){
+				if(MK.navs[i]=='|'){
+					texts+='<b>|</b>';
+				}else{
+					texts+='<span class="mk-'+MK.navs[i]+'" title="'+MK.navs[i]+'"><i class="icono-'+MK.navs[i]+'"></i></span>';
+				}
+			}
+			return texts;
+		},
+		DisPlay(box){
+			M=document.getElementById(box);
+			M.className=M.className==''?'mk-editor':M.className+' mk-editor';
+			var elem = document.createElement('div');
+			var nav = document.createElement('div');
+			elem.className='mk-box';
+			nav.className='mk-nav';
+			nav.innerHTML=MK.getNavList();
+			M.parentNode.replaceChild(elem,M);
+			elem.appendChild(nav);
+			elem.appendChild(M);
+		},
 	};
-	
 })(window);
 
