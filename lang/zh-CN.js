97172b5f86689d5502024dcefcdc7af9ad68b000
IDEASHU <32724294+amuke123@users.noreply.github.com>
2025-06-26 18:39:29 +0800

    up

--- lang/zh-CN.js
+++ lang/zh-CN.js
@@ -75,6 +75,6 @@ var langs={
 	uploadError:'上传失败文件',
 	
 	name:'MukeEditor',
-	introduce:'开发原因：由于现有富文本编辑器不是体量太大就是对HTML5中"video"和"audio"支持不好，所以才决定着手写了这个富文本编辑器。<br/>编辑器特点：MukeEditor是一款所见即所得富文本web编辑器，具有轻量，使用便利，可定制，多语言支持，注重用户体验等特点。<br/>开发感言：开发初期由于对富文本编辑器了解非常少导致进度非常缓慢，初始版本断断续续开发共耗时约有一周左右，希望能对网站开发者有所帮助。',
+	introduce:'编辑器特点：MukeEditor是一款所见即所得富文本web编辑器，具有轻量，使用便利，可定制，多语言支持，注重用户体验等特点，希望能对网站开发者有所帮助。',
 	copyright:'Copyright © <a target="_blank" href="http://www.amuker.com">amuker.com</a> All rights reserved.',
 };
